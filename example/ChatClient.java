1a1d5d4e51c55448f00061629ea505d99d4196c4
David Rohmer <DavidRohmer@web.de>
2011-11-21 01:22:27 +0100

    -made ChatClient example more useful

--- example/ChatClient.java
+++ example/ChatClient.java
@@ -8,7 +8,9 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
@@ -16,135 +18,144 @@ import javax.swing.JTextField;
 import net.tootallnate.websocket.Draft;
 import net.tootallnate.websocket.WebSocketClient;
 import net.tootallnate.websocket.drafts.Draft_10;
-
-/**
- * A barebones chat client that uses the WebSocket protocol.
- */
-public class ChatClient extends WebSocketClient {
-    private final JTextArea ta;
-
-    public ChatClient(URI uri, JTextArea ta, Draft draft) {
-        super(uri, draft);
-        this.ta = ta;
-    }
-
-    public void onMessage(String message) {
-        ta.append(message + "\n");
-    }
-
-    public void onOpen() {
-        ta.append("You are connected to ChatServer: " + getURI() + "\n");
-    }
-
-    public void onClose() {
-        ta.append("You have been disconnected from: " + getURI() + "\n");
-    }
-
-    public void onIOError(IOException ex) {
-        ta.append("Network problem ...\n");
-    }
-
-    /**
-     * The JFrame for our Chat client.
-     */
-    private static class Frame extends JFrame implements ActionListener {
-        private static final long serialVersionUID = -6056260699202978657L;
-        
-        private final JTextField uriField;
-        private final JButton connect;
-        private final JButton close;
-        private final JTextArea area;
-        private final JTextField chatField;
-        private ChatClient cc;
-
-        public Frame() {
-            super("WebSocket Chat Client");
-            Container c = getContentPane();
-            GridLayout layout = new GridLayout();
-            layout.setColumns(1);
-            layout.setRows(5);
-            c.setLayout(layout);
-
-            uriField = new JTextField();
-            uriField.setText("ws://localhost:8887");
-            c.add(uriField);
-
-            connect = new JButton("Connect");
-            connect.addActionListener(this);
-            c.add(connect);
-            
-            close = new JButton("Close");
-            close.addActionListener(this);
-            c.add(close);
-
-            JScrollPane scroll = new JScrollPane();
-            area = new JTextArea();
-            scroll.setViewportView(area);
-            c.add(scroll);
-
-            chatField = new JTextField();
-            chatField.setText("");
-            chatField.addActionListener(this);
-            c.add(chatField);
-
-            java.awt.Dimension d = new java.awt.Dimension(300, 400);
-            setPreferredSize(d);
-            setSize(d);
-
-            addWindowListener(new java.awt.event.WindowAdapter() {
-                @Override
-                public void windowClosing(WindowEvent e) {
-                    if (cc != null) {
-                        try {
-                            cc.close();
-                        } catch (IOException ex) { ex.printStackTrace(); }
-                    }
-                    Frame.this.dispose();
-                }
-            });
-
-            setLocationRelativeTo(null);
-            setVisible(true);
-        }
-
-        public void actionPerformed(ActionEvent e) {
-
-            if (e.getSource() == chatField) {
-                if (cc != null) {
-                    try {
-                        cc.send(chatField.getText());
-                        chatField.setText("");
-                        chatField.requestFocus();
-                    } catch (IOException ex) { ex.printStackTrace(); }
-                }
+import net.tootallnate.websocket.drafts.Draft_75;
+import net.tootallnate.websocket.drafts.Draft_76;
 
 
-            } else if (e.getSource() == connect) {
-                connect.setEnabled(false);
-                uriField.setEditable(false);
-                try {
-                    cc = new ChatClient(new URI(uriField.getText()), area, new Draft_10() );
-                    cc.connect();
-                } catch (URISyntaxException ex) {
-                    area.append(uriField.getText() + " is not a valid WebSocket URI\n");
-                    connect.setEnabled(true);
-                    uriField.setEditable(true);
-                }
-            } else if (e.getSource() == close) {
-                try {
-                    cc.close();
-                } catch(Exception ex) {
-                  ex.printStackTrace();
-                }
-            }
-        }
-    }
+public class ChatClient extends JFrame implements ActionListener {
+	private static final long serialVersionUID = -6056260699202978657L;
 
-    public static void main(String[] args) {
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Frame();
-            }
-        });
-    }
+	private final JTextField uriField;
+	private final JButton connect;
+	private final JButton close;
+	private final JTextArea ta;
+	private final JTextField chatField;
+	private final JComboBox draft;
+	private WebSocketClient cc;
+
+	public ChatClient() {
+		super("WebSocket Chat Client");
+		Container c = getContentPane();
+		GridLayout layout = new GridLayout();
+		layout.setColumns(1);
+		layout.setRows(6);
+		c.setLayout(layout);
+
+		Draft[] drafts =  { new Draft_75(), new Draft_76(), new Draft_10() };
+		draft = new JComboBox( drafts );
+		c.add(draft);
+
+		uriField = new JTextField();
+		uriField.setText("ws://localhost:8887");
+		c.add(uriField);
+
+		connect = new JButton("Connect");
+		connect.addActionListener(this);
+		c.add(connect);
+
+		close = new JButton("Close");
+		close.addActionListener(this);
+		close.setEnabled( false );
+		c.add(close);
+		
+
+		JScrollPane scroll = new JScrollPane();
+		ta = new JTextArea();
+		scroll.setViewportView(ta);
+		c.add(scroll);
+
+		chatField = new JTextField();
+		chatField.setText("");
+		chatField.addActionListener(this);
+		c.add(chatField);
+
+		java.awt.Dimension d = new java.awt.Dimension(300, 400);
+		setPreferredSize(d);
+		setSize(d);
+
+		addWindowListener(new java.awt.event.WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				if (cc != null) {
+					try {
+						cc.close();
+					} catch (IOException ex) { 
+						ex.printStackTrace(); 
+					}
+				}
+				dispose();
+			}
+		});
+
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+
+		if (e.getSource() == chatField) {
+			if (cc != null) {
+				try {
+					cc.send(chatField.getText());
+					chatField.setText("");
+					chatField.requestFocus();
+				} catch (IOException ex) { ex.printStackTrace(); }
+			}
+
+		} else if (e.getSource() == connect) {
+			try {
+				//cc = new ChatClient(new URI(uriField.getText()), area, ( Draft ) draft.getSelectedItem() );
+				cc = new WebSocketClient( new URI(uriField.getText()),( Draft ) draft.getSelectedItem()) {
+
+					public void onMessage(String message) {
+						ta.append( "got: " + message + "\n");
+						ta.setCaretPosition( ta.getDocument().getLength() );
+					}
+
+					public void onOpen() {
+						ta.append("You are connected to ChatServer: " + getURI() + "\n");
+						ta.setCaretPosition( ta.getDocument().getLength() );
+					}
+
+					public void onClose() {
+						ta.append("You have been disconnected from: " + getURI() + "\n");
+						ta.setCaretPosition( ta.getDocument().getLength() );
+						connect.setEnabled(true);
+						uriField.setEditable(true);
+						draft.setEditable(true);
+						close.setEnabled( false );
+					}
+
+					public void onIOError(IOException ex) {
+						ta.append("Network problem ...\n"+ex+"\n");
+						ta.setCaretPosition( ta.getDocument().getLength() );
+						ex.printStackTrace();
+						connect.setEnabled(true);
+						uriField.setEditable(true);
+						draft.setEditable(true);
+						close.setEnabled( false );
+					}
+				};
+				
+				close.setEnabled( true);
+				connect.setEnabled(false);
+				uriField.setEditable(false);
+				draft.setEditable(false);
+				cc.connect();
+			} catch (URISyntaxException ex) {
+				ta.append(uriField.getText() + " is not a valid WebSocket URI\n");
+			}
+		} else if (e.getSource() == close) {
+			try {
+				cc.close();
+			} catch(Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		new ChatClient();
+	}
+
 }
