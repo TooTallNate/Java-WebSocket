ce3ede175dad731d43f095867b4e4ee1e1b0fc4f
Nathan Rajlich <nathan@tootallnate.net>
2012-03-04 16:36:07 -0800

    ChatClient: make Draft 17 be the default selection.

--- example/ChatClient.java
+++ example/ChatClient.java
@@ -41,7 +41,7 @@ public class ChatClient extends JFrame implements ActionListener {
 		layout.setRows( 6 );
 		c.setLayout( layout );
 
-		Draft[] drafts = { new Draft_10(), new Draft_17(), new Draft_76(), new Draft_75() };
+		Draft[] drafts = { new Draft_17(), new Draft_10(), new Draft_76(), new Draft_75() };
 		draft = new JComboBox( drafts );
 		c.add( draft );
 
