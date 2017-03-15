dd4323d3fda3e2d2b97298c5cd407695de2fe2f4
Felix von Ferey <felixvf@users.noreply.github.com>
2017-03-15 16:21:45 +0000

    Fix bug https://github.com/TooTallNate/Java-WebSocket/issues/373. (#374)
    Deadlock due to race condition in WebSocketServer.stop

--- src/main/java/org/java_websocket/server/WebSocketServer.java
+++ src/main/java/org/java_websocket/server/WebSocketServer.java
@@ -219,8 +219,8 @@ public abstract class WebSocketServer extends WebSocketAdapter implements Runnab
 
 		synchronized ( this ) {
 			if( selectorthread != null && selectorthread != Thread.currentThread() ) {
-				selector.wakeup();
 				selectorthread.interrupt();
+				selector.wakeup();
 				selectorthread.join( timeout );
 			}
 		}
