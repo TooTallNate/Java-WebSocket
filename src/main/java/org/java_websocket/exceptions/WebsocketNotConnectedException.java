ae0fe9e03d719d53fe9c81d14cfdd54ce4b839f6
Marcel P <marci4@live.de>
2017-03-23 09:48:10 +0100

    Added javadocs for the exceptions

--- src/main/java/org/java_websocket/exceptions/WebsocketNotConnectedException.java
+++ src/main/java/org/java_websocket/exceptions/WebsocketNotConnectedException.java
@@ -1,5 +1,19 @@
 package org.java_websocket.exceptions;
 
+/**
+ * exception which indicates the websocket is not yet connected (READYSTATE.OPEN)
+ */
 public class WebsocketNotConnectedException extends RuntimeException {
 
+    /**
+     * Serializable
+     */
+    private static final long serialVersionUID = -785314021592982715L;
+
+    /**
+     * constructor for a WebsocketNotConnectedException
+     */
+    public WebsocketNotConnectedException() {
+        super();
+    }
 }
