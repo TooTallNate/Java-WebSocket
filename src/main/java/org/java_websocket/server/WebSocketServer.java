6ff85c410571a09be15ee52eb5ff544c9ff7af99
Paul Morrison <paul.morrison@rogers.com>
2020-12-06 19:40:42 -0500

    Update WebSocketServer.java

--- src/main/java/org/java_websocket/server/WebSocketServer.java
+++ src/main/java/org/java_websocket/server/WebSocketServer.java
@@ -722,7 +722,8 @@ public abstract class WebSocketServer extends AbstractWebSocket implements Runna
   @Override
   public final void onWebsocketClose(WebSocket conn, int code, String reason, boolean remote) {
     // fix: reason display suppressed by removeConnection(conn) or onClose(...)
-		log.error(reason);   // added
+	  if (reason != null)                      // added
+		log.error("Reason: " + reason);   // added
     selector.wakeup();
     try {
       if (removeConnection(conn)) {
